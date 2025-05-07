092ffdf884ceffc142ed62e9058de8f674593f9a
IrvingCorrosk19 <irvingcorrosk19@gmail.com>
2025-05-07 00:22:39 -0500

    Actualización de configuración para backend en Render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // ✅ Esto abrirá el navegador automáticamente
-    port: 5173  // (opcional) puedes cambiar el puerto si es necesario
+    open: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://inventory-app-backend-jzkd.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
